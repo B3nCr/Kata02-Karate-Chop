a51d56dad978a46fea227318b4ce4f5b3dfaf811
Ben Crinion <ben.crinion@gmail.com>
2019-03-10 23:44:27 +0000

    Failing test for first approach

---
+++ BinarySplit.Tests/BinarySplitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace BinarySplit.Tests
+{
+    public class BinarySplitTest
+    {
+        [Fact]
+        public void CanAdd()
+        {
+            var splitter = new BinarySpliter();
+            var index = splitter.chop(1, new[] { 1, 2, 3, 4, 5, 6 });
+            index.Should().Be(1);
+        }
+    }
+
+    internal class BinarySpliter
+    {
+        public int chop(int valueToFind, int[] array)
+        {
+            return -1;
+        }
+    }
+}
