0361c046f6bfe7ca8d963b443af6eca429a714f8
Ben Crinion <ben.crinion@gmail.com>
2019-03-10 23:50:52 +0000

    Looking for existing item failing

--- BinarySplit.Tests/BinarySplitTest.cs
+++ BinarySplit.Tests/BinarySplitTest.cs
@@ -8,28 +8,30 @@ namespace BinarySplit.Tests
     {
         [Theory]
         [InlineData(-1, 3, new int[0])]
-//   assert_equal(-1, chop(3, [1]))
-//   assert_equal(0,  chop(1, [1]))
-//   assert_equal(0,  chop(1, [1, 3, 5]))
-//   assert_equal(1,  chop(3, [1, 3, 5]))
-//   assert_equal(2,  chop(5, [1, 3, 5]))
-//   assert_equal(-1, chop(0, [1, 3, 5]))
-//   assert_equal(-1, chop(2, [1, 3, 5]))
-//   assert_equal(-1, chop(4, [1, 3, 5]))
-//   assert_equal(-1, chop(6, [1, 3, 5]))
-//   assert_equal(0,  chop(1, [1, 3, 5, 7]))
-//   assert_equal(1,  chop(3, [1, 3, 5, 7]))
-//   assert_equal(2,  chop(5, [1, 3, 5, 7]))
-//   assert_equal(3,  chop(7, [1, 3, 5, 7]))
-//   assert_equal(-1, chop(0, [1, 3, 5, 7]))
-//   assert_equal(-1, chop(2, [1, 3, 5, 7]))
-//   assert_equal(-1, chop(4, [1, 3, 5, 7]))
-//   assert_equal(-1, chop(6, [1, 3, 5, 7]))
-//   assert_equal(-1, chop(8, [1, 3, 5, 7]))
+        [InlineData(-1, 3, new int[] { 1 })]
+        [InlineData(0, 1,  new int[] { 1 })]
+        //   assert_equal(0,  chop(1, [1, 3, 5]))
+        //   assert_equal(1,  chop(3, [1, 3, 5]))
+        //   assert_equal(2,  chop(5, [1, 3, 5]))
+        //   assert_equal(-1, chop(0, [1, 3, 5]))
+        //   assert_equal(-1, chop(2, [1, 3, 5]))
+        //   assert_equal(-1, chop(4, [1, 3, 5]))
+        //   assert_equal(-1, chop(6, [1, 3, 5]))
+        //   assert_equal(0,  chop(1, [1, 3, 5, 7]))
+        //   assert_equal(1,  chop(3, [1, 3, 5, 7]))
+        //   assert_equal(2,  chop(5, [1, 3, 5, 7]))
+        //   assert_equal(3,  chop(7, [1, 3, 5, 7]))
+        //   assert_equal(-1, chop(0, [1, 3, 5, 7]))
+        //   assert_equal(-1, chop(2, [1, 3, 5, 7]))
+        //   assert_equal(-1, chop(4, [1, 3, 5, 7]))
+        //   assert_equal(-1, chop(6, [1, 3, 5, 7]))
+        //   assert_equal(-1, chop(8, [1, 3, 5, 7]))
         public void CanAdd(int expected, int search, int[] array)
         {
             var splitter = new BinarySpliter();
+
             var index = splitter.chop(search, array);
+
             index.Should().Be(expected);
         }
     }
