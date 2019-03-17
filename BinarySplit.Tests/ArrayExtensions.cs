022efd00aa629fe24051e19d7b1eff57e9aefa02
Ben Crinion <ben.crinion@gmail.com>
2019-03-17 19:35:27 +0000

    Cleanup solution

---
+++ BinarySplit.Tests/ArrayExtensions.cs
@@ -0,0 +1,46 @@
+﻿namespace BinarySplit.Tests
+{
+    public static class ArrayExtensions
+    {
+        public static int BinarySearch(this int[] array, int searchFor, int startIndex)
+        {
+            var mid = array.Length / 2;
+            var current = array[mid];
+
+            // Check weather the element in the middle is the element we are search for
+            if (current == searchFor)
+            {
+                return mid + startIndex;
+            }
+
+            if (array.Length == 1)
+            {
+                return -1;
+            }
+
+            if (current > searchFor)
+            {
+                return array
+                    .Split(0, mid - 1)
+                    .BinarySearch(searchFor, startIndex);
+            }
+
+            return array.Split(mid, array.Length -1).BinarySearch(searchFor, mid);
+        }
+
+        public static int[] Split(this int[] array, int left, int right)
+        {
+            var length = right - left + 1;
+            var newArray = new int[length];
+            var newIndex = 0;
+
+            for (var i = left; i <= right; i++)
+            {
+                newArray[newIndex] = array[i];
+                newIndex++;
+            }
+
+            return newArray;
+        }
+    }
+}
---
+++ BinarySplit.Tests/FunctionalSearcher.cs
@@ -0,0 +1,15 @@
+﻿namespace BinarySplit.Tests
+{
+    public class FunctionalSearcher : ISearcher
+    {
+        public int FindIndex(int search, int[] array)
+        {
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+
+            return array.BinarySearch(search, 0);
+        }
+    }
+}
